955d0cbca001551cd30c4e7433c75e455510ed9d
sukhpaul <sukhpaul.nijjar@gmail.com>
2017-10-23 21:16:08 -0700

    App.js rendering

--- src/views/app.js
+++ src/views/app.js
@@ -1,14 +1,24 @@
+// Inside of src/views/app.js, modify the code that will 
+// render this view to the DOM. Not all of the tests in 
+// appSpec are pertinent at this time. Be sure to follow 
+// the MVP practice, that is writing the minimum code at
+// each step.
+
 var AppView = Backbone.View.extend({
 
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.render();
+    this.videos = new Videos(window.exampleVideoData);
   },
-
 
   render: function() {
     this.$el.html(this.template());
+    new VideoListView({
+      el: this.$('.list'),
+      collection: this.videos
+    }).render();
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,11 +1,27 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function () {
+    this.collection.on('sync', function() {
+      this.render();
+    }, this);
+  },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    
+    var $item = this.$el;
+    $('.video-list').children().empty();
+    this.collection.forEach(function(video) {
+      var videoView = new VideoListEntryView({model: video}).render();
+      $item.append(videoView);
+    });
     return this;
+    
+    
+    
   },
+  
 
   template: templateURL('src/templates/videoList.html')
 
